b1a197c9645c84bf1d3beec736cab333220644ff
erbatyr <erbatyr.akyn.97@gmail.com>
2020-12-05 14:36:09 +0600

    adding hibernate for this project

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,9 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.HibernateSessionFactoryUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -12,31 +15,59 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        String sql = "CREATE TABLE IF NOT EXISTS users(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR, lastname VARCHAR, age INT)";
+        session.createSQLQuery(sql).addEntity(User.class);
+        transaction.commit();
     }
 
     @Override
     public void dropUsersTable() {
-
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        String sql = "DROP TABLE IF EXISTS users";
+        session.createSQLQuery(sql).executeUpdate();
+        transaction.commit();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        User user = new User(name, lastName, age);
+        session.save(user);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Object user1 =session.get(User.class, id);
+        if(user1 !=null){
+            session.delete(user1);
+        }
+        transaction.commit();
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        List<User> users = session.createQuery("from User").list();
+        transaction.commit();
+        session.close();
+        return users;
+
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.createQuery("delete from User").executeUpdate();
+        transaction.commit();
     }
 }
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,14 +1,17 @@
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.sql.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class MyTest {
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-		UserServiceImpl userService = new UserServiceImpl();
-		userService.saveUser("Jackie", "Chan", (byte) 66);
-		userService.saveUser("Bruce", "Willis", (byte) 65);
-		userService.saveUser("Jamie", "Foxx", (byte) 52);
-		userService.saveUser("Jeremy", "Renner", (byte) 49);
+//		UserServiceImpl userService = new UserServiceImpl();
+//		userService.saveUser("Jackie", "Chan", (byte) 66);
+//		userService.saveUser("Bruce", "Willis", (byte) 65);
+//		userService.saveUser("Jamie", "Foxx", (byte) 52);
+//		userService.saveUser("Jeremy", "Renner", (byte) 49);
 //		UserServiceTest userServiceTest = new UserServiceTest();
 //		userServiceTest.createUsersTable();
 //		userServiceTest.saveUser();
@@ -22,5 +25,10 @@ public class MyTest {
 //
 //		UserServiceImpl userService = new UserServiceImpl();
 //		userService.createUsersTable();
+
+		UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+//		userDaoHibernate.createUsersTable();
+//		userDaoHibernate.saveUser("Goss", "Varshowsky", (byte) 14);
+		userDaoHibernate.dropUsersTable();
 	}
 }
